33fefa67f581a0d7ff762c8d1d9c52ba3233d697
Jorge Argueta <34260059+Arg45@users.noreply.github.com>
2019-12-18 17:42:40 -0600

    avance

--- Music++/[EDD]Proyecto1-201404334/abrir.cpp
+++ Music++/[EDD]Proyecto1-201404334/abrir.cpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "abrir.h"
+#include "EDD/Artistas.h"
+#include "EDD/Albumes.h"
+#include "EDD/Canciones.h"
 #include <fstream>
 
 using namespace std;
@@ -42,7 +45,30 @@ bool Abrir::tipoCorrecto(string url) {
 	return (url.substr(url.find_last_of(".") + 1) == "json") ? true : false;
 }
 void Abrir::leerLibreria(Json::Value libreria) {
-
+	Json::Value artista, album, cancion;
+	float promedioAlbum, promedioCancion, ratingArtista, ratingAlbum, ratingCancion;
+	int cantAlbums, cantCanciones;
+	string nombreArtista, nombreAlbum, nombreCancion, mes, anio;
+	int i;
+	for (i = 0; i < libreria.size(); i++) {
+		nombreArtista = libreria[i]["Artist"]["Name"].asString();
+		artista = libreria[i]["Artist"]["Albums"];
+		promedioAlbum = 0.0;
+		cantAlbums = 0;
+		Albumes* a = new Albumes();
+		int j;
+		for (j = 0; j < artista.size(); j++) {
+			nombreAlbum = artista[j]["Name"].asString();
+			mes = artista[j]["Month"].asString();
+			anio = artista[j]["Year"].asString();
+			album = artista[j]["Songs"];
+			cantAlbums++;
+			promedioCancion = 0.0;
+			cantCanciones = 0;
+			Canciones* c = new Canciones();
+			for
+		}
+	}	
 }
 void Abrir::leerListaR(Json::Value libreria, string nombre) {
 
